24bc78597e032091ef1c37873047c8a944cd108a
墨石 <582307236@qq.com>
2020-11-11 13:11:54 +0800

    Style optimization

--- src/Components/TreeSelect/index.jsx
+++ src/Components/TreeSelect/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
+import './index.css';
 
 export default function TreeSelect({ values }) {
   const node = useRef();
